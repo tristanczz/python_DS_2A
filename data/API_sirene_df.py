f1955b3400f7272a197844186191a11f4f3010e3
tobiasxcz <czarnecki.tristan@gmail.com>
2025-12-05 16:23:28 +0100

    .

--- data/API_sirene_df.py
+++ data/API_sirene_df.py
@@ -7,38 +7,6 @@ API_KEY = "2e19c44e-91b4-4c19-99c4-4e91b40c193f"
 BASE_URL = "https://api.insee.fr/api-sirene/3.11/siret"
 
 
-def query_sirene0(date_min="2024-01-01", date_max="2024-12-31", limit=1000):
-    url = "https://api.insee.fr/api-sirene/3.11/siret"
-    query = f"dateCreationEtablissement:[{date_min} TO {date_max}]"
-
-    headers = {
-        "X-INSEE-Api-Key-Integration": API_KEY
-    }
-
-    params = {
-        "q": query,
-        "nombre": limit
-    }
-
-    r = requests.get(url, headers=headers, params=params)
-
-    if r.status_code != 200:
-        raise Exception(f"Erreur API ({r.status_code}) : {r.text}")
-
-    data = r.json()
-    etabs = data.get("etablissements", [])
-
-    df = pd.DataFrame([{
-        "siret": e.get("siret"),
-        "date_creation": e.get("dateCreationEtablissement"),
-        "region": e["periodesEtablissement"][0].get("regionImplantationEtablissement"),
-        "activite": e["periodesEtablissement"][0].get("activitePrincipaleEtablissement")
-    } for e in etabs])
-
-    df["date_creation"] = pd.to_datetime(df["date_creation"], errors="coerce")
-
-    return df
-
 
 def query_sirene(year, limit=5000):
     """
@@ -74,15 +42,14 @@ def query_sirene(year, limit=5000):
 
     return df
 
-def query_sirene_rnd(year, region=None, limit=5000):
+def query_sirene_rnd(year, limit=5000):
     url = "https://api.insee.fr/api-sirene/3.11/siret"
 
     # Filtre NAF R&D
-    query_parts = ['activitePrincipaleEtablissement:72']
+    query_parts = ['activitePrincipaleEtablissement:72*']
     if year:
         query_parts.append(f'dateCreationEtablissement:[{year}-01-01 TO {year}-12-31]')
-    if region:
-        query_parts.append(f'regionImplantationEtablissement:{region}')
+
 
     q = " AND ".join(query_parts)
     q_encoded = quote(q)  # encode les caractères spéciaux
@@ -102,8 +69,8 @@ def query_sirene_rnd(year, region=None, limit=5000):
     df = pd.DataFrame([{
         "siret": e.get("siret"),
         "date_creation": e.get("dateCreationEtablissement"),
-        "region": e["periodesEtablissement"][0].get("regionImplantationEtablissement"),
-        "naf": e["periodesEtablissement"][0].get("activitePrincipaleEtablissement")
+        "code_postal": e.get("Adresse", {}).get("codePostalEtablissement"),
+        "naf": e["periodesEtablissement"][0].get("activitePrincipaleEtablissement"),
     } for e in etabs])
 
     df["date_creation"] = pd.to_datetime(df["date_creation"], errors="coerce")
@@ -113,6 +80,9 @@ def query_sirene_rnd(year, region=None, limit=5000):
 
 
 
-
-df = query_sirene(2023, limit=5000)
+# exemple d'utilisation
+#df = query_sirene(2023, limit=5000)
+#df.to_csv("sirene_rnd_2023_test.csv", index=False)
+df = query_sirene_rnd(2023, limit=5000)
 df.to_csv("sirene_rnd_2023_test.csv", index=False)
+
